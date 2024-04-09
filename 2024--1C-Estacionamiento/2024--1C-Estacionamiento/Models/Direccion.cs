73c62c1d3ef89f8542c111303f107a87ffc98177
KARINA AUDAY <karina.auday@ort.edu.ar>
2024-04-08 21:46:16 -0300

    Creacion de Modelos

---
+++ 2024--1C-Estacionamiento/2024--1C-Estacionamiento/Models/Direccion.cs
@@ -0,0 +1,17 @@
+﻿namespace _2024__1C_Estacionamiento.Models
+{
+    public class Direccion
+    {
+        public int Id { get; set; }
+
+        public string Calle { get; set; }
+
+        public int Numero { get; set; }
+
+        public string Localidad { get; set; }
+
+        public string Provincia { get; set; }
+
+        public string Pais { get; set; }    
+    }
+}
---
+++ 2024--1C-Estacionamiento/2024--1C-Estacionamiento/Models/Telefono.cs
@@ -0,0 +1,13 @@
+﻿namespace _2024__1C_Estacionamiento.Models
+{
+    public class Telefono
+    {
+        public int Id { get; set; }
+
+        public int CodArea { get; set; }
+
+        public string Numero { get; set; }
+
+
+    }
+}
