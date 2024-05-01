84036fa298fe5b36919f3f5f735c4eae84f7fec8
KARINA AUDAY <karina.auday@ort.edu.ar>
2024-05-01 17:03:42 -0300

    Agrego imagenes, startup, colores, y practica con Formulario en crecion de Personas

---
+++ 2024--1C-Estacionamiento/2024--1C-Estacionamiento/Controllers/PersonaController.cs
@@ -0,0 +1,43 @@
+﻿using _2024__1C_Estacionamiento.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2024__1C_Estacionamiento.Controllers
+{
+    public class PersonaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //Creo Persona con Get
+        public ActionResult CreateGet()   //Ejemplo Con Get
+        {
+
+
+            return View();
+
+        }
+        //Creo Persona con Post
+        public ActionResult CreatePost() //Ejemplo con Post
+        {
+
+
+            return View();
+
+        }
+        //Creo persona con query string
+        public IActionResult CrearPersona(String nombre, String apellido)
+        {
+            Persona persona = new Persona()
+
+            {
+                Apellido = apellido,
+                // Dni = dni,
+                Nombre = nombre,
+            };
+
+            return View(persona);
+
+        }
+    }
+}
---
+++ 2024--1C-Estacionamiento/2024--1C-Estacionamiento/Startup.cs
@@ -0,0 +1,67 @@
+﻿using _2024__1C_Estacionamiento.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+
+namespace _2024__1C_Estacionamiento
+{
+    public class Startup
+    {
+
+        public static WebApplication InicializarApp(string[] args)
+        {
+            //Crear una nueva instancia de nuestro Servidor Web
+            var builder = WebApplication.CreateBuilder(args);
+
+            //Agrego Servicios. El contexot de base de datos le agrego el Database in memory. Expreciones Lambda
+
+            //  builder.Services.AddDbContext<EstacionamientoContext>(options => options.UseInMemoryDatabase("EstacionamientoDb"));
+
+            ////Agrego la base de datos SQL , y guardo el conection string en el appsetting.json
+            //builder.Services.AddDbContext<EstacionamientoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("EstacionamientoDBCS")));
+
+
+            builder.Services.AddControllersWithViews();
+
+            ConfigureServices(builder);//Lo configuramos con sus respectivos servicios
+
+            var app = builder.Build();
+
+            Configure(app);
+
+            return app;
+
+        }
+        private static void ConfigureServices(WebApplicationBuilder builder)
+        {
+
+            builder.Services.AddControllersWithViews();
+        }
+
+        private static void Configure(WebApplication app)
+        {
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            //Agrego autenticacion
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        }
+
+    }
+}
+
